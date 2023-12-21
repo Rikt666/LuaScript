a0fb7a675f0df2e1a25b10f16b0ac67850ae822e
Rikt666 <154546588+Rikt666@users.noreply.github.com>
2023-12-22 02:58:57 +0800

    Update first github.lua

--- first github.lua
+++ first github.lua
@@ -1,2 +1,1 @@
-T = load(gg.makeRequest("https://gist.githubusercontent.com/Rikt666/ad05df442d87947341f5b2758b653330/raw/95eacc30e3ece2d6753b39b6ed654470768c503f/luaScriptGist.lua").content)
-pcall(T)
+gg.alert("TEST")
