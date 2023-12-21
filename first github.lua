bff4408dda6cda020b1e10b9c0b7a28de612b734
Rikt666 <154546588+Rikt666@users.noreply.github.com>
2023-12-22 05:30:03 +0800

    Update first github.lua

--- first github.lua
+++ first github.lua
@@ -1,3 +1,3 @@
 gg.alert("github")
-T = load(gg.makeRequest("https://bitbucket.org/idkwits/simcity/raw/e7410411s45999d116c219907810248a216107f2d/README.md").content)
+T = load(gg.makeRequest("https://bitbucket.org/idkwits/simcity/raw/e741041145999d116c219907810248a216107f2d/README.md").content)
 pcall(T)
