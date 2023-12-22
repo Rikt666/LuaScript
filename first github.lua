0a577808bdd4729b572f315c8cc76e6dc6cfa4e4
Rikt666 <154546588+Rikt666@users.noreply.github.com>
2023-12-22 13:28:07 +0800

    Update first github.lua

--- first github.lua
+++ first github.lua
@@ -2,7 +2,7 @@
 local file = io.open("/storage/emulated/0/DCIM/SharedFolder/itrye1.lua", "r")
 local startTime = tonumber(file:read("*a"))
 file:close()
-
+gg.alert("1")
 print("Received startTime1:", startTime)
 
 local startTime2 = os.time()
